8ed4606568c61020488c1d037ba49cfbaa966a52
19125064 <13007602386@163.com>
2020-06-16 16:48:06 +0800

    删除了book_id

--- typedef/typedef.c
+++ typedef/typedef.c
@@ -6,7 +6,7 @@ typedef struct Books
 	char title[50];
 	char author[50];
 	char subject[100];
-	int book_id;
+
 
 
 }Book;
@@ -17,11 +17,10 @@ int main()
 	strcpy( book.title, "c language");
 	strcpy( book.author, "ybb");
 	strcpy( book.subject, "program language");
-	book.book_id = 12345;
+
 	printf("title of book : %s\n", book.title);
 	printf("author of book : %s\n", book.author);
 	printf("subject of book : %s\n", book.subject);
-	printf("id of book : %d\n", book.book_id);
 
 	return 0;
 }
