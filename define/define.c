8ae38038f203232f2c71e35d680e02082b5c2067
19125064 <13007602386@163.com>
2020-06-16 17:54:39 +0800

    删除了book_id

--- define/define.c
+++ define/define.c
@@ -6,7 +6,7 @@ typedef struct Books
 	char  title[50];
 	char  author[50];
 	char  subject[100];
-	int   book_id;
+
 } Book;
 
 int main()
@@ -16,12 +16,12 @@ int main()
 	strcpy(book.title, "C 教程");
 	strcpy(book.author, "Runoob");
 	strcpy(book.subject, "编程语言");
-	book.book_id = 12345;
+
 
 	printf("书标题 : %s\n", book.title);
 	printf("书作者 : %s\n", book.author);
 	printf("书类目 : %s\n", book.subject);
-	printf("书 ID : %d\n", book.book_id);
+
 
 	return 0;
 }
